605fe29f173e6fe6cc18ddda48bfc7f8ed93a41c
Nikolay <nikolayivanov944@gmail.com>
2025-03-28 20:28:27 +0300

    Init hm

--- src/main/java/services/StudentService.java
+++ src/main/java/services/StudentService.java
@@ -1,17 +1,18 @@
 package ru.hogwarts.school.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.NotFoundException;
 import ru.hogwarts.school.models.Student;
 import ru.hogwarts.school.repositories.StudentRepository;
-
 import java.util.List;
-
 @Service
 public class StudentService {
-
     private final StudentRepository students;
     private final FacultyService faculties;
+
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentService(StudentRepository students, FacultyService faculties) {
         this.students = students;
@@ -19,27 +20,37 @@ public class StudentService {
     }
 
     public Student addStudent(Student student) {
+        logger.info("addStudent: add student");
+
         student.setId(null);
         reSetFaculty(student);
         return students.save(student);
     }
 
     private void reSetFaculty(Student student) {
+        logger.info("reSetFaculty: reset faculty for student");
+
         student.setFaculty(faculties.getFaculty(student.getFaculty().getId()));
     }
 
     public Student getStudent(long id) {
+        logger.info("getStudent: id {}", id);
+
         checkExistsId(id);
         return students.findById(id).orElseThrow();
     }
 
     public Student updateStudent(Student student) {
+        logger.info("updateStudent: update student");
+
         checkExistsId(student.getId());
         reSetFaculty(student);
         return students.save(student);
     }
 
     public Student deleteStudent(long id) {
+        logger.info("deleteStudent: delete student, id {}", id);
+
         Student student = getStudent(id);
         students.delete(student);
         return student;
@@ -47,34 +58,29 @@ public class StudentService {
 
     private void checkExistsId(long id) {
         if (!students.existsById(id)) {
+            logger.error("Student with id {} not found", id);
+
             throw new NotFoundException("Student with id " + id + " not found");
         }
     }
-
     public List<Student> getAllStudents() {
         return students.findAll();
     }
-
     public List<Student> findStudentsByAge(int age) {
         return students.findByAge(age);
     }
-
     public List<Student> findStudentsByAge(int min, int max) {
         return students.findByAgeBetween(min, max);
     }
-
     public List<Student> findStudentsByFacultyId(Long facultyId) {
         return students.findStudentsByFacultyId(facultyId);
     }
-
     public Integer getCountOfStudents() {
         return students.getCountOfStudents();
     }
-
     public Integer getAvgAgeOfStudents() {
         return students.getAvgAgeOfStudents();
     }
-
     public List<Student> getLastFiveStudents() {
         return students.getLastFiveStudents();
     }
